8665cc3bd9dcb02feb737d7ac93c71e39618d4aa
Russell Mutamba <russell.mutamba@students.uz.ac.zw>
2025-12-02 18:47:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5652502183-51a04",
+  "appId": "1:298452329862:web:6aad12df36351d4a30b807",
+  "apiKey": "AIzaSyCReXyImml6JS3u6gcMWr2YV_NG2V3vcso",
+  "authDomain": "studio-5652502183-51a04.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "298452329862"
+};
